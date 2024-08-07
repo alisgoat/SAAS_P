8b61f7ed8cb0e951e9912217f253dcf619054a97
ali <ali@example.com>
2024-08-07 12:47:57 +0530

    Copied all the changes

--- src/saashome/views.py
+++ src/saashome/views.py
@@ -5,6 +5,10 @@ from visits.models import PageVisit
 def home_page_view(request,*args,**kwargs):
     queryset = PageVisit.objects.all()
     #return HttpResponse("<h1>Hello World</h1>")
+    page="My page"
+    page_o ={
+        "get_page" : page
+    }
     template = "home.html"
     PageVisit.objects.create()
-    return render (request, template)
+    return render (request, template,page_o)
